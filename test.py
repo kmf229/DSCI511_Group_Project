583f1cad5adff225a083231eef3ae55afc005129
Justin Goheen <j.goheen@protonmail.com>
2019-09-25 06:24:09 -0400

    adding some basic files and updating structure

---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import unittest
+from scraping import *
+from main import *
+
+
+
+# this file will be used to test methods/functions
